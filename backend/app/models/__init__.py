af5391c950e845f8f68e64a3b8028ef390a37707
mksjy <160725687+mksjy@users.noreply.github.com>
2025-05-09 13:13:27 +0900

    feat : 토큰블락리스트 모델 import

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from employee import Employee, EmployeeHealth, Admin, EmergencyContact
 from device import Device, DeviceManagement, DeviceFailure
 from measurement import DeviceMeasurement, HealthAnomaly
 from health import EmployeeHealth, EmergencyContact, HealthAnomaly
+from token import TokenBlocklist
