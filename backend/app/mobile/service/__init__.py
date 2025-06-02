80ae4b30e84bf0ed09dadbdf5fa44f72a69d83c1
Ninoling <binah_w2h@naver.com>
2025-06-02 14:10:57 +0000

    DeviceSercive 추가

--- backend/app/mobile/service/__init__.py
+++ backend/app/mobile/service/__init__.py
@@ -1,5 +1,6 @@
 # 서비스 모듈 초기화
 from .auth_service import AuthService
 from .profile_service import ProfileService
+from .device_service import DeviceService
 
-__all__ = ['AuthService', 'ProfileService']
+__all__ = ['AuthService', 'ProfileService',  'DeviceService']
